6bd328527a4039a2a2e280ba604b6217f63d0c4b
Adriano Costa <66333662+dricocosta@users.noreply.github.com>
2022-06-13 23:52:23 -0300

    Add files via upload
    Adicionando aulas do bootcamp

---
+++ Aulas/2 - Aulas JavaScript/aula 6 - aprofundando funções/script.js
@@ -0,0 +1,23 @@
+// tipos de funções
+
+// declarativas
+function funcao() {
+    console.log('Tudo certo, jovem!')
+}
+
+funcao();
+
+// expressão de função com nomeação (opcional)
+var funcao = function funcao() {
+    console.log('Sou uma mensagem de função de expressão!')
+}
+funcao();
+
+// arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function.')
+}
+funcao();
+
+// Funções aritméticas
+
