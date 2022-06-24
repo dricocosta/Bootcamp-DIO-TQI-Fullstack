9c907e66e18fd338645cb40cbdfc50e2595e7936
dricocosta <cfadriano@outlook.com>
2022-06-24 17:40:42 -0300

    Adicionando exercícios em Java

---
+++ Exercício Java - Calculadora/src/digitalInnovation/Main.java
@@ -0,0 +1,17 @@
+package src.digitalInnovation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercício calculadora");
+        calculadora.soma(3, 6);
+        calculadora.subtracao(9, 1.8);
+        calculadora.multiplicacao(7, 8);
+        calculadora.divisao(5, 2.5);
+
+                
+    }
+    
+}
